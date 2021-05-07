2def0eebcb005d3e98769743beab191ad555e7f6
Kumo <63556081+Mysttt@users.noreply.github.com>
2021-05-07 21:52:14 +0100

    Beta

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 # Imports
+import asyncio
+from os import listdir
+
 from discord.ext import commands
 from discord.ext.commands import CommandNotFound, MissingRequiredArgument, CommandInvokeError, MissingRole
 import discord
@@ -22,6 +25,7 @@ client = commands.Bot(command_prefix=config['Prefix'], intents=discord.Intents.a
 client.remove_command('help')
 
 # sends discord logging files which could potentially be useful for catching errors.
+os.remove("Logs/logs.txt")
 FORMAT = '[%(asctime)s]:[%(levelname)s]: %(message)s'
 logging.basicConfig(filename='Logs/logs.txt', level=logging.DEBUG, format=FORMAT)
 logging.debug('Started Logging')
@@ -34,45 +38,14 @@ async def on_ready():
     config_activity = config['bot_activity']
     activity = discord.Game(name=config['bot_status_text'])
     logging.info('Getting Bot Activity from Config')
-    print("You're currently running a beta version! -- If you encounter any bugs, let me know.")
+    print("If you encounter any bugs, please let me know.")
     print('------')
-    print('Logged In:')
-    print(f"Bot Username: {client.user.name}\nBotID: {client.user.id}")
+    print('Logged In As:')
+    print(f"Username: {client.user.name}\nID: {client.user.id}")
     print('------')
-    print(f"Set Status To: {config_status}\nSet Activity To: {config_activity}")
-    print("------")
-    print("Started System: Levels")
+    print(f"Status: {config_status}\nActivity: {config_activity}")
+    print('------')
     await client.change_presence(status=config_activity, activity=activity)
-    logging.info('Logged In And Set Activity')
-
-
-# If enabled in config, will send a welcome message + adds a role if a new user joins the guild (if roles are enabled).
-@client.event
-async def on_member_join(member):
-    if config['join_leave_message'] is True:
-        logging.info('A user has joined a guild')
-        channel = client.get_channel(config['join_leave_channel'])
-        embed = discord.Embed(title=f"**:man_raising_hand: WELCOME**", description=f"Welcome **{member.mention}** to **{member.guild.name}**!", colour=discord.Colour.green())
-        embed.set_author(name=member.name, icon_url=member.avatar_url)
-        embed.set_thumbnail(url=member.guild.icon_url)
-        await channel.send(embed=embed)
-    if config['add_role'] is True:
-        rank = discord.utils.get(member.guild.roles, name=config['on_join_role'])
-        await member.add_roles(rank)
-        print(f"User: {member} was given the {rank} role.")
-        logging.info('A role was sent to a user')
-
-
-# If enabled in config, will send a leave message if a user leaves the guild
-@client.event
-async def on_member_remove(member):
-    if config['join_leave_message'] is True:
-        logging.info('A user left a guild')
-        channel = client.get_channel(config['join_leave_channel'])
-        embed = discord.Embed(title=f"**:man_raising_hand: GOODBYE**", description=f"**{member.mention}** has left **{member.guild.name}**!", colour=discord.Colour.red())
-        embed.set_author(name=member.name, icon_url=member.avatar_url)
-        embed.set_thumbnail(url=member.guild.icon_url)
-        await channel.send(embed=embed)
 
 
 @client.event  # Stops Certain errors from being thrown in the console (Don't remove as it'll cause command error messages to not send! - Only remove if adding features and needed for testing (Don't forget to re-add)!)
@@ -89,37 +62,21 @@ async def on_command_error(ctx, error):
     if isinstance(error, MissingRole):
         logging.error('A user has missing roles!')
         return
-    if isinstance(error, AttributeError):
-        logging.error('Attribute Error')
+    if isinstance(error, KeyError):
+        logging.error('Key Error')
         return
     raise error
 
-logging.info('Checking if anti-spam is enabled')
-if spamconfig['antispam_system'] is True:
-    client.load_extension("Systems.spamsys")
-    logging.info('Loaded AntiSpam')
-logging.info('Checking if help is enabled')
-if config['help_command'] is True:
-    client.load_extension("Commands.help")
-    logging.info('Loaded Help Command')
+logging.info("------------- Loading -------------")
+for fn in listdir("Commands"):
+    if fn.endswith(".py"):
+        logging.info(f"Loading {fn}")
+        client.load_extension(f"Commands.{fn[:-3]}")
+        logging.info(f"Loaded {fn}")
+logging.info(f"Loading Level System")
 client.load_extension("Systems.levelsys")
-logging.info('Loaded Levelsys')
-client.load_extension("Commands.rank")
-logging.info('Loaded Rank Command')
-client.load_extension("Commands.leaderboard")
-logging.info('Loaded Leaderboard Command')
-client.load_extension("Commands.background")
-logging.info('Loaded Background Command')
-client.load_extension("Commands.reset")
-logging.info('Loaded Reset Command')
-client.load_extension("Commands.circlepic")
-logging.info('Loaded Circlepic Command')
-client.load_extension("Commands.xpcolour")
-logging.info('Loaded XPColour Command')
-client.load_extension("Commands.fix")
-logging.info('Loaded Fix Command')
-client.load_extension("Commands.shutdown")
-logging.info('Loaded Shutdown Command')
+logging.info(f"Loaded Level System")
+logging.info("------------- Finished Loading -------------")
 
 # Uses the bot token to login, so don't remove this.
 token = os.getenv("DISCORD_TOKEN")
