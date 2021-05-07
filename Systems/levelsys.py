94b55e71311c4f9cabf415be6626cfd8848b3257
Kumo <63556081+Mysttt@users.noreply.github.com>
2021-05-07 21:52:44 +0100

    Beta

--- Systems/levelsys.py
+++ Systems/levelsys.py
@@ -1,6 +1,5 @@
 # Imports
 import asyncio
-
 import discord
 from discord.ext import commands
 from pymongo import MongoClient
@@ -26,9 +25,6 @@ with open("Configs/config.yml", "r", encoding="utf-8") as file:
 with open("Configs/spamconfig.yml", "r", encoding="utf-8") as file2:
     spamconfig = yaml.load(file2)
 
-# Some config options which need to be stored here, again, no need for altering.
-level_roles = config['level_roles']
-level_roles_num = config['level_roles_num']
 
 # Vac-API, no need for altering!
 vac_api = vacefron.Client()
@@ -40,54 +36,100 @@ class levelsys(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, ctx):
-        stats = levelling.find_one({"id": ctx.author.id})
+        stats = levelling.find_one({"guildid": ctx.guild.id, "id": ctx.author.id})
+        serverstats = levelling.find_one({"server": ctx.guild.id})
         if not ctx.author.bot:
+            if serverstats is None:
+                newserver = {"server": ctx.guild.id, "xp_per_message": 10, "double_xp_role": "NA", "level_channel": "private"}
+                overwrites = {
+                    ctx.guild.default_role: discord.PermissionOverwrite(read_messages=False),
+                    ctx.guild.me: discord.PermissionOverwrite(read_messages=True)
+                }
+                await ctx.guild.create_text_channel('private', overwrites=overwrites)
+                levelling.insert_one(newserver)
+                member = ctx.author
+                serverstat = levelling.find_one({"server": ctx.guild.id})
+                channel = discord.utils.get(member.guild.channels, name=serverstat["level_channel"])
+                prefix = config['Prefix']
+                await channel.send(f"> Hey!\n\n You will only see this message **once**. To change the channel where levelup messages get sent to:\n\n`{prefix}levelchannel <channelname>` -- Please do NOT use the hashtag and enter any -'s!\n\nYou can also set a role which earns 2x XP by doing the following:\n\n`{prefix}doublexp <rolename>`\n\nYou can also add or remove roles after levelling up by doing the following\n\n`{prefix}role <add|remove> <level> <rolename>`\n\nYou can also change how much xp you earn per message by doing:\n\n`{prefix}xppermessage <amount>`\n\nFor help with commands:\n\n`{prefix}help` ")
             if stats is None:
-                newuser = {"id": ctx.author.id, "tag": ctx.author.mention, "xp": 0, "rank": 1, "background": " ", "circle": False, "xp_colour": "#ffffff", "name": f"{ctx.author}", "pfp": f"{ctx.author.avatar_url}"}
+                newuser = {"guildid": ctx.guild.id, "id": ctx.author.id, "tag": ctx.author.mention, "xp": serverstats["xp_per_message"], "rank": 1, "background": " ", "circle": False, "xp_colour": "#ffffff", "name": f"{ctx.author}", "pfp": f"{ctx.author.avatar_url}"}
                 print(f"User: {ctx.author.id} has been added to the database! ")
                 levelling.insert_one(newuser)
             else:
                 if config['Prefix'] in ctx.content:
-                    stats = levelling.find_one({"id": ctx.author.id})
+                    stats = levelling.find_one({"guildid": ctx.guild.id, "id": ctx.author.id})
                     xp = stats["xp"]
-                    levelling.update_one({"id": ctx.author.id}, {"$set": {"xp": xp}})
+                    levelling.update_one({"guildid": ctx.guild.id, "id": ctx.author.id}, {"$set": {"xp": xp}})
                 else:
-                    user = ctx.author
-                    role = discord.utils.get(ctx.guild.roles, name=config['double_xp'])
-                    if role in user.roles:
-                        xp = stats["xp"] + config['xp_per_message'] * 2
-                        levelling.update_one({"id": ctx.author.id}, {"$set": {"xp": xp}})
+                    stats = levelling.find_one({"server": ctx.guild.id})
+                    if stats is None:
+                        return
                     else:
-                        xp = stats["xp"] + config['xp_per_message']
-                        levelling.update_one({"id": ctx.author.id}, {"$set": {"xp": xp}})
-                levelling.update_one({"id": ctx.author.id}, {'$set': {"pfp": f"{ctx.author.avatar_url}", "name": f"{ctx.author}"}})
+                        user = ctx.author
+                        role = discord.utils.get(ctx.guild.roles, name=serverstats["double_xp_role"])
+                    if role in user.roles:
+                        stats = levelling.find_one({"guildid": ctx.guild.id, "id": ctx.author.id})
+                        xp = stats["xp"] + serverstats['xp_per_message'] * 2
+                        levelling.update_one({"guildid": ctx.guild.id, "id": ctx.author.id}, {"$set": {"xp": xp}})
+                    else:
+                        stats = levelling.find_one({"guildid": ctx.guild.id, "id": ctx.author.id})
+                        xp = stats["xp"] + serverstats['xp_per_message']
+                        levelling.update_one({"guildid": ctx.guild.id, "id": ctx.author.id}, {"$set": {"xp": xp}})
+                levelling.update_one({"guildid": ctx.guild.id, "id": ctx.author.id}, {'$set': {"pfp": f"{ctx.author.avatar_url}", "name": f"{ctx.author}"}})
                 lvl = 0
                 while True:
                     if xp < ((config['xp_per_level'] / 2 * (lvl ** 2)) + (config['xp_per_level'] / 2 * lvl)):
                         break
                     lvl += 1
                 xp -= ((config['xp_per_level'] / 2 * ((lvl - 1) ** 2)) + (config['xp_per_level'] / 2 * (lvl - 1)))
-                if xp == 0:
-                    channel = self.client.get_channel(config['level_message_channel'])
-                    levelling.update_one({"id": ctx.author.id}, {"$set": {"rank": + config['xp_per_message'] * 2}})
+                if stats["xp"] < 0:
+                    levelling.update_one({"guildid": ctx.guild.id, "id": ctx.author.id}, {"$set": {"xp": 0}})
+                if stats["rank"] != lvl:
+                    levelling.update_one({"guildid": ctx.guild.id, "id": ctx.author.id}, {"$set": {"rank": lvl + 1}})
                     embed2 = discord.Embed(title=f":tada: **LEVEL UP!**",
                                            description=f"{ctx.author.mention} just reached Level: **{lvl}**",
                                            colour=config['embed_colour'])
                     xp = stats["xp"]
+                    levelling.update_one({"guildid": ctx.guild.id, "id": ctx.author.id},
+                                         {"$set": {"rank": lvl, "xp": xp + serverstats['xp_per_message'] * 2}})
                     print(f"User: {ctx.author} | Leveled UP To: {lvl}")
                     embed2.add_field(name="Next Level:",
-                                     value=f"``{int(config['xp_per_level'] * 2 * ((1 / 2) * lvl))}xp``")
+                                     value=f"`{int(config['xp_per_level'] * 2 * ((1 / 2) * lvl))}xp`")
                     embed2.set_thumbnail(url=ctx.author.avatar_url)
+                    member = ctx.author
+                    channel = discord.utils.get(member.guild.channels, name=serverstats["level_channel"])
+                    if config['level_up_ping'] is True:
+                        await channel.send(f"{ctx.author.mention}")
+                    msg = await channel.send(embed=embed2)
+                    level_roles = serverstats["role"]
+                    level_roles_num = serverstats["level"]
                     for i in range(len(level_roles)):
                         if lvl == level_roles_num[i]:
                             await ctx.author.add_roles(
                                 discord.utils.get(ctx.author.guild.roles, name=level_roles[i]))
-                            embed2.add_field(name="Role Unlocked:", value=f"`{level_roles[i]}`")
+                            embed = discord.Embed(title=":tada: **LEVEL UP**",
+                                                  description=f"{ctx.author.mention} just reached Level: **{lvl}**",
+                                                  colour=config['embed_colour'])
+                            embed.add_field(name="Next Level:",
+                                            value=f"`{int(config['xp_per_level'] * 2 * ((1 / 2) * lvl))}xp`")
+                            embed.add_field(name="Role Unlocked", value=f"`{level_roles[i]}`")
                             print(f"User: {ctx.author} | Unlocked Role: {level_roles[i]}")
-                            embed2.set_thumbnail(url=ctx.author.avatar_url)
-                            await channel.send(embed=embed2)
-                        else:
-                            await channel.send(embed=embed2)
+                            embed.set_thumbnail(url=ctx.author.avatar_url)
+                            await msg.edit(embed=embed)
+                        for i in range(len(level_roles)):
+                            if lvl == level_roles_num[i]:
+                                await ctx.author.add_roles(
+                                    discord.utils.get(ctx.author.guild.roles, name=level_roles[i]))
+                                embed = discord.Embed(title=":tada: **LEVEL UP**",
+                                                      description=f"{ctx.author.mention} just reached Level: **{lvl}**",
+                                                      colour=config['embed_colour'])
+                                embed.add_field(name="Next Level:",
+                                                 value=f"`{int(config['xp_per_level'] * 2 * ((1 / 2) * lvl))}xp`")
+                                embed.add_field(name="Role Unlocked", value=f"`{level_roles[i]}`")
+                                print(f"User: {ctx.author} | Unlocked Role: {level_roles[i]}")
+                                embed.set_thumbnail(url=ctx.author.avatar_url)
+                                await msg.edit(embed=embed)
 
 
 def setup(client):
