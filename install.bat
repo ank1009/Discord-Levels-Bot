454326fb130bf5c65fe777c27aa3a49528291db0
Kumo <63556081+Mysttt@users.noreply.github.com>
2021-06-15 13:56:53 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,8 +1,3 @@
 @echo off
-pip install discord
-pip install ruamel.yaml
-pip install pymongo
-pip install dnspython
-python -m pip install vacefron.py -U
-pip install python-dotenv
+pip install -r requirements.txt
 pause
